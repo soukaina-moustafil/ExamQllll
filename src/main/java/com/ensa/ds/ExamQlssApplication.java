051da2060792438b4f3229e21cde5618bc2fdb7f
soukaina <moustafilsoukaina@gmail.com>
2021-12-19 14:01:47 +0100

    init project

---
+++ src/main/java/com/ensa/ds/ExamQlssApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQlssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQlssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensa/ds/ExamQlssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQlssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
